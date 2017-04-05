88561a5b846055640a626fbc04fd2dd407fef589
Chin Yong Kian <chinykian@hotmail.com>
2017-04-06 00:06:22 +0800

    Revert "Revert "Added solution for projectEuler Q1""
    This reverts commit 8dbef09a94eb4f6f686542b21508e52ca75e95cc.

---
+++ 001/001/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // find all multiples of 3 below 1000
+            List<int> mul = new List<int>();
+            for (int i=3; i<1000; ++i)
+            {
+                if (i%3 == 0)
+                    mul.Add(i);
+            }
+
+            // find all multiples of 5 that isn't a multiple of 3
+            for (int i = 5; i < 1000; ++i)
+            {
+                if (i%5 == 0 && i%3 != 0)
+                    mul.Add(i);
+            }
+
+            int sum = 0;
+            foreach (int elem in mul)
+            {
+                sum += elem;
+            }
+
+            Console.WriteLine("SUM: " + sum);
+        }
+    }
+}
