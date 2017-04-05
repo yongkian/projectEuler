ff76097e9282567f0d92a1d1a95afbb83f83b510
Chin Yong Kian <chinykian@hotmail.com>
2017-04-06 00:34:30 +0800

    Soln 2 added.

---
+++ 002/002/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int prev = 1;
+            int curr = 2;
+            int sum = curr;
+            int store = 0;
+            while ((store = prev+curr) <= 4000000)
+            {
+                prev = curr;
+                curr = store;
+
+                if (curr % 2 == 0)
+                    sum += curr;
+            }
+            Console.WriteLine("SUM: " + sum);
+        }
+    }
+}
