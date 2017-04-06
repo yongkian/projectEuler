728a28152ebc6240eb00ef1c4795067feeb6c54d
Chin Yong Kian <chinykian@hotmail.com>
2017-04-07 00:05:01 +0800

    004, 005

---
+++ 004/004/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong store = 0;
+            ulong largestPalindrome = 0;
+            for (ulong lhs=999; lhs>=100; --lhs)
+            {
+                for (ulong rhs=999; rhs>=100; --rhs)
+                {
+                    store = lhs * rhs;
+                    if (isPalindrome(store) && store > largestPalindrome)
+                    {
+                        largestPalindrome = store;
+                    }
+                }
+            }
+
+            Console.WriteLine("Palindrome: " + largestPalindrome);
+        }
+
+        static bool isPalindrome(ulong val)
+        {
+            string valstring = val.ToString();
+            string reverseString = new string(valstring.ToCharArray().Reverse().ToArray());
+            
+            return valstring == reverseString;
+        }
+    }
+}
---
+++ 005/005/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+ * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int num = 1;
+
+            while (true)
+            {
+                for (i=1; i<=20; ++i)
+                {
+                    if (num % i != 0)
+                        break;
+                }
+                if (i == 21)
+                    break;
+                ++num;
+            }
+
+            Console.WriteLine("Answer: " + num);
+        }
+    }
+}
